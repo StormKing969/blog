3b5f2b68172969af1c85ecb1e9144419ce62d2eb
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-07-08 10:56:49 -0500

    Trending Post SwiperJS

--- components/trending.js
+++ components/trending.js
@@ -2,17 +2,40 @@ import Image from "next/image";
 import Link from "next/link";
 import Author from "./child/author";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+
 export const trending = () => {
+
   const bg = {
     // background: "url('/images/banner.png') no-repeat",
     // backgroundPosition: "right"
-    backgroundColor: "#E9FFF1"
+    backgroundColor: "#E9FFF1",
   };
+
   return (
     <section className="py-16" style={bg}>
       <div className="container mx-auto md:px-20">
         <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
-        {Slide()}
+
+        <Swiper
+          slidesPerView={1}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
@@ -33,6 +56,7 @@ function Slide() {
           </a>
         </Link>
       </div>
+
       <div className="info flex justify-center flex-col">
         <div className="category">
           <Link href={"/"}>
@@ -42,6 +66,7 @@ function Slide() {
             <a className="text-gray-800 hover:text-gray-600"> - July 3, 2016</a>
           </Link>
         </div>
+
         <div className="title">
           <Link href={"/"}>
             <a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
@@ -49,10 +74,12 @@ function Slide() {
             </a>
           </Link>
         </div>
+
         <p className="text-gray-900 py-3">
           What do it mean to be fluent in a language? Over the years, the
           language that we speak evolves and adapts to what the masses
         </p>
+
         <Author></Author>
       </div>
     </div>
